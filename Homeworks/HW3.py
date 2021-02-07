46c0f7c05b81aee7e6f58822e6956dd8ac830fc8
aybukedalman <78547014+aybukedalman@users.noreply.github.com>
2021-02-07 23:45:19 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,13 @@
 #Explain your work
 
 #Question 1
+for i in range(1, 100):
+    count = 0
+    for j in range(1, i + 1):
+        if (i % j == 0):
+            count = count + 1
+    if(count == 2):
+        print(i,end="  ")
 for x in range(a):
 	print(a)
+	2  3  5  7  11  13  17  19  23  29  31  37  41  43  47  53  59  61  67  71  73  79  83  89  97  
